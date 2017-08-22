d50e3bcba8d8f68fbf89922c95e932545e732d48
asydor94 <asydor94@gmail.com>
2017-08-22 16:43:49 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#create a matrix object that caches its inverse for future reference 
+ 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) #creates a function where the argument is a matrix 
+    { 
+    inv <- NULL #initializes a variable that will hold the value of the matrix's inverse 
+    set <- function(y) 
+        {  
+    x <<- y #set x to the y argument in function 
+    inv <<- NULL 
+        }
+    
+ get <- function() x #define the get function to return the value of the matrix 
+ setinverse <- function(inverse) inv <<- inverse #sets the inverse value 
+ getinverse <- function() inv #gets the value of the inverse 
+ list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) #returns the values for 'get', 'setinverse', 'getinverse'
+    } 
+ 
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+ 
+ cacheSolve <- function(x, ...)  #crete a function to compute the inverse of the matrix if it is not cached
+     { 
+       inv <- x$getinverse() #sets the value equal to the inverse found from previous formula
+       if(!is.null(inv)) 
+           { #if the inverse is not null (i.e - a cached inverse exists) 
+             message("getting cached data") #show that RStudio is pulling the cached matrix 
+             return(inv) #return the matrix inverse 
+           } 
+       #if the inverse is null (not previously cached) 
+       data <- x$get() #get the matrix data 
+       inv <- solve(data, ...) #take the inverse of the matrix 
+       x$setinverse(inv) #set the inverse of the matrix to the inv variable
+       inv #return the inverse of the matrix 
+     } 
